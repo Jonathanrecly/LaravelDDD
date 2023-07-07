53b9278b5aa226bc10878fbb327a45a1e31b9584
Jonathan Recly <j.recly@gmail.com>
2023-07-07 15:10:00 +0400

    (refacto): add contract

--- app/Domain/User/UserController.php
+++ app/Domain/User/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Domain\User;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 interface UserController
 {
+    public function index(Request $request): AnonymousResourceCollection;
+
+    public function show(string $uuid): JsonResource;
+
     public function store(StoreUserRequest $request): JsonResource;
+
+    public function update(UpdateUserRequest $request, string $uuid): JsonResource;
 }
